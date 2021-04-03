b8db6b4d1948f70fb85bdf5e0dfc95a4a826ec7c
kfd <ikfdla13@gmail.com>
2021-04-03 10:28:57 +0700

    first commit

---
+++ modulrestapiphp/server/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+ define('HOST','localhost');
+ define('USER','id16442373_latteace');
+ define('PASS','F~X%(s@~t%zy0D*$');
+ define('DB','id16442373_restaurant');
+
+ $con = new mysqli(HOST,USER,PASS,DB) or die('Unable to Connect');
